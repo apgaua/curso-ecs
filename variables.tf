636a07f5d5d37ec72bae7d081cbb299a7500f4ac
apgaua <apgaua.sousa@gmail.com>
2025-04-01 14:48:47 -0300

    Deploy first version

---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "project_name" {
+  type = string
+}
+
+variable "default_tags" {
+  type = map(string)
+  default = {
+    contato = ""
+    dia     = ""
+    repo    = ""
+  }
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "vpc_additional_cidrs" {
+  type        = list(string)
+  description = "Additional VPC CIDRs"
+  default     = []
+}
+
+##################################################
+############### PUBLIC SUBNET ####################
+##################################################
+
+variable "publicsubnets" {
+  type        = list(string)
+  description = "Public subnet values"
+}
+
+##################################################
+############### PRIVATE SUBNET ###################
+##################################################
+
+variable "privatesubnets" {
+  type        = list(string)
+  description = "Private subnet values"
+}
+
+##################################################
+################ DATABASE SUBNET #################
+##################################################
+
+variable "databasesubnets" {
+  type        = list(string)
+  description = "Database subnet values"
+}
+
+##################################################
+############### DATABASE NACL ####################
+##################################################
+
+variable "database_nacl_rules" {
+  # type = list(object({
+  #   rule_start_number = number
+  #   rule_action       = string
+  #   protocol          = string
+  #   from_port         = optional(number)
+  #   to_port           = optional(number)
+  # }))
+  type        = list(map(string))  
+  description = "ACL rule to database subnet"
+}
